0b63148ccd8b1307603a63c7243e6861ce06ba48
Nosta <nosta@macbook-air-nosta.local>
2018-06-21 00:29:03 +0300

    Initial Commit

---
+++ structures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  structures
+//
+//  Created by Nosta on 21.06.2018.
+//  Copyright © 2018 Nosta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
